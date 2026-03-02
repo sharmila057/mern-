9eade17229df568e6a8250d9c4941eb1be74d8ad
sharmila <sharmilareddy1030@gmail.com>
2026-03-02 10:37:18 +0530

    js completed

--- javascript/script.js
+++ javascript/script.js
@@ -1,207 +1,357 @@
-// ------------------ BASICS ------------------
+// console.log("Hello world");
+// //Reintialiaze and Redeclare
+// var a;//Global scope
+// a=10;
+// var a;
+// a=20;
+// //Reintialize
+// let b=10;//block scope
 
-console.log("Hello World");
+// //can't Redeclare and Reintialize
+// const c=30;
 
-// var, let, const
-var a;
-a = 10;
-var a;   // allowed with var
-a = 20;
+// console.log(a);
+// console.log(b);
+// console.log(c);
 
-let b = 10;   // block scope
-b = 20;       // reassign allowed
+// var num=10;
+// var str="venkey";//we can use `` | "" |''
+// var bool=true;
+// var un;
+// var nu=null;
+// var big=32132444222n;
+// var symbol=Symbol('li');
 
-const c = 30; // cannot reassign
+// console.log(typeof num);
+// console.log(typeof str);
+// console.log(typeof bool);
+// console.log(typeof un);
+// console.log(typeof nu);
+// console.log(typeof big);
+// console.log(typeof symbol);
 
-console.log(a);
-console.log(b);
-console.log(c);
+// var arr=[1,2,3,4,5];
+// console.log(arr);
+// console.log(typeof arr);
 
-// ------------------ DATA TYPES ------------------
+// var obj={name:'venkey',dept:'DS'};
+// console.log(obj);
+// console.log(typeof obj);
 
-var num = 10;
-var str = "thanu";
-var bool = true;
-var un;
-var nu = null;
-var big = 123456789079n;
-var symbol = Symbol("li");
+// console.log("Hello World");
+// var a;
+// a=10;
+// var a;
+// a=20;
+// let b=10;
+// //let b;
+// const c=30;
+// console.log(a);
+// console.log(b);
+// console.log(c);
 
-console.log(typeof num);
-console.log(typeof str);
-console.log(typeof bool);
-console.log(typeof un);
-console.log(typeof nu);   // object (JS bug)
-console.log(typeof big);
-console.log(typeof symbol);
 
-// Array
-var arr = [1,2,3,4,5];
-console.log(arr);
-console.log(typeof arr); // object
-console.log(Array.isArray(arr)); // true
+//logical (&&,||,!)
+// var a=true;
+// var b=false;
+// console.log(a&&b);
+// console.log(a||b);
+// console.log(!a);
 
-// Object
-var Obj = {
-    name: "revanth",
-    dept: "ds"
-};
-console.log(Obj);
-console.log(typeof Obj);
+// //control statement
+// //conditional statement(if,if...else,else if,switch)
+// //if(even)
+// var a=10;
+// if(a%2===0){
+//     console.log("Even");
+// }
 
-// ------------------ ARITHMETIC OPERATORS ------------------
+// //if...else
+// if(a%2==0){
+//     console.log(`${a} is Even`);
+// }
+// else{
+//     console.log(`${a} is Odd`);
+// }
+// //else if
+// var mark=80;
+// if(mark>90){
+//     console.log("O grade");
+// }
+// else if(mark>70){
+//     console.log("A grade");
+// }
+// else if(mark>35){
+//     console.log("Pass");
+// }
+// else{
+//     console.log("Fail");
+// }
 
-var x = 10;
-var y = "2";
+//Ternary operator (condition? true : false)
+// var c=5;
+// const result=(c%2==0) ?`${c} is Even`:`${c} is Odd`;
+// console.log(result);
 
-console.log(x + y);
-console.log(x - y);
-console.log(x % y);
-console.log(x * y);
-console.log(x ** y);
+// var mark=80;
+// const grade=(mark>90) ? `O grade` : (mark>70) ? `A grade` : (mark>35) ? `Pass` : `Fail`;
+// console.log(grade);
 
-// ------------------ RELATIONAL OPERATORS ------------------
+//switch
+// var day=5;
+// switch(day){
+//     case 1:
+//         console.log("sunday");
+//         break;
+//     case 2:
+//         console.log("monday");
+//         break;
+//     case 3:
+//         console.log("Tuesday");
+//         break;
+//     case 4:
+//         console.log("wednesday");
+//         break;
+//     case 5:
+//         console.log("Thursday");
+//         break;
+//     case 6:
+//         console.log("Friday");
+//         break;
+//     case 7:
+//         console.log("saturday");
+//         break;
+//     default:
+//         console.log("invalid");
+// }
 
-var r1 = 10;
-var r2 = 10;
+//loops(for,while,do while)
+// for loop
+// for(let i=1;i<=10;i++){
+//     console.log(i)
+// }
 
-console.log(r1 == r2);
-console.log(r1 === r2);
-console.log(r1 != r2);
-console.log(r1 !== r2);
-console.log(r1 > r2);
-console.log(r1 < r2);
-console.log(r1 <= r2);
-console.log(r1 >= r2);
+//while loop
+// var num=1234;
+// var count=0;
+// while(num>0){
+//     count++;
+//     console.log(num);
+//     num=Math.floor(num/10);
+// }
+// console.log(count);
 
-// ------------------ LOGICAL OPERATORS ------------------
+//do while loop
+// var a=0;
+// do{
+//     console.log("Example do while");
+// }while(a!=0);
 
-var l1 = true;
-var l2 = true;
+//normal function
+// function add(a,b){
+//     // var a=10;
+//     // var b=20;
+//     console.log(a+b);
+// }
+// add(10,20);
 
-console.log(l1 && l2);
-console.log(l1 || l2);
-console.log(!l2);
+// //arrow function
+// var add=()=>{
+//     console.log("It is arrow function");
+// }
+// add();
+// //with parameter
+// var add=(a,b)=>{
+//     console.log(a+b);
+// }
+// add(10,20);
+// //with return
+// var add=(a,b)=>{
+//     return a+b;
+// }
+// console.log(add(10,20));
 
-// ------------------ CONDITIONAL STATEMENTS ------------------
+// //call back
+// var add=(a,b,demo)=>{
+//     demo(a+b);
+// }
+// var result=(res)=>{
+//     console.log(res);
+// }
+// add(10,20,result);
+// //Example-1
+// var add=(a,b,demo)=>{
+//     var c=a+b;
+//     demo(c);
+// }
+// add(10,20,(res)=>{
+//     console.log(res);
+// });
+//Example-2
+// var add=(a,b,demo)=>{
+//     var c=a+b;
+//     demo(c);
+// }
+// add(10,20,(res)=>{
+//     console.log(res);
+// });
+// add(10,20,(res)=>{
+//     for(let i=1;i<=res;i++){
+//         if(i%2==0){
+//             console.log(i);
+//         }
+//     }
+// });
 
-var num1 = 10;
+// //spread operator(...) (ES6)
+// var arr=[1,2,3];
+// var arr2=[...arr,4,5,6];
+// console.log(arr);
+// console.log(arr2);
 
-if(num1 % 2 === 0){
-    console.log("Even");
-}
-else{
-    console.log("Odd");
-}
+// //Destructuring operator (ES6)
+// var [m1,m2,m3,m4,m5]=[99,98,97,96,95];
+// console.log(m1);
+// console.log(m2);
+// console.log(m3);
+// console.log(m4);
+// console.log(m5);
 
-var marks = 80;
+// //
+// var {name,age,dept,contact} ={
+//     name:'venkey',
+//     age:20,
+//     dept:["DS","AI"],
+//     contact:{mobile:9392837185,mail:'venkeyr162@gmaol.com'}
+// }
+// console.log(name);
+// console.log(age);
+// console.log(dept);
+// console.log(contact);
 
-if(marks > 90){
-    console.log("O+ Grade");
-}
-else if(marks > 80){
-    console.log("A Grade");
-}
-else if(marks > 70){
-    console.log("B Grade");
-}
-else{
-    console.log("Fail");
-}
+// //for..in
+// var arr=[10,20,30,40];////Array
+// for(let i in arr){
+//     console.log(i,arr[i]);
+// }
+// var obj={ //object
+//     name:'venkey',
+//     age:20,
+//     isActive:true
+// }
+// for(let key in obj){
+//     console.log(key);
+// }
 
-// Ternary
-var value = 5;
-const result1 = (value % 2 === 0) ? `${value} is even` : `${value} is odd`;
-console.log(result1);
+// //for..of
+// var arr=[10,20,30,40]; //Array
+// for(let value of arr){
+//     console.log(value);
+// }
+// //forEach
+// arr.forEach((index,val)=>{
+//     console.log(index,val);
+// })
 
-// Switch
-var day = 6;
-switch(day){
-    case 1: console.log("Sunday"); break;
-    case 2: console.log("Monday"); break;
-    case 3: console.log("Tuesday"); break;
-    case 4: console.log("Wednesday"); break;
-    case 5: console.log("Thursday"); break;
-    case 6: console.log("Friday"); break;
-    case 7: console.log("Saturday"); break;
-    default: console.log("Invalid Day");
-}
+// //Map
+// var arr=[1,2,3,4,5];
+// var double=arr.map((val)=>(val*2));
+// console.log(double);
+// //filter
+// var arr=[1,2,3,4,5];
+// var even=arr.filter((val)=>(val%2==0))
+// console.log(even);
+// //reduce
+// var total=arr.reduce((sum,val)=>(sum+val),0);
+// console.log(total);
+// //Example
+// var result=arr.map((val)=>(val**2)).filter((val)=>val%2==0).reduce((sum,val)=>(sum+val),0);
+// console.log(result);
 
-// ------------------ LOOPS ------------------
+// //hosting
+// console.log(a);
+// var a;
+// //function hosting
+// add();
+// function add(){
+//     console.log(10+20);
+// }
 
-for(let i=0; i<=5; i++){
-    console.log(i);
-}
+// //global scope
+// //var
+// var a=10;  //global scope
+// if(true){
+//     var b=20; //block scope
+// }
+// console.log(a);
+// console.log(b);
 
-var number = 1234;
-var count = 0;
-while(number > 0){
-    count++;
-    number = Math.floor(number/10);
-}
-console.log("Digit count:", count);
+// //const
+// const i=10; //global scope
+// if(true){
+//     const j=20; //block scope
+// }
+// console.log(i);
+// console.log(j);
 
-// ------------------ FUNCTIONS ------------------
+//---------
+//promise
+// const promise=new Promise((resolve,reject)=>{
+//     var success=true;
+//     if(success){
+//         console.log("Task completd");
+//     }
+//     else{
+//         console.log("Task not completd");
+//     }
+// })
+// promise.then((message)=>{
+//     console.log(message);
+// }).catch((err)=>{
+//     console.log(err);
+// })
 
-// Normal function
-function add1(a,b){
-    return a+b;
-}
-console.log(add1(10,20));
+// //giving time 
+// const promise =new Promise((resolve,reject)=>{
+//     var success=true;
+//     if(success){
+//         setTimeout(()=>{
+//             resolve("Task completed");
+//         },2000);
+//     }
+//     else{
+//         reject("Task not cokmpleted");
+//     }
+// })
+// promise.then((msg)=>console.log(msg))
+// .catch((err)=>console.log(err));
 
-// Arrow function
-const add2 = (a,b) => a+b;
-console.log(add2(5,15));
+// //setInterval
+// var count=1;
+// const IntervalCount = setInterval(()=>{
+//     console.log(count);
+//     count++;
+//     if(count>5){
+//         clearInterval(IntervalCount);
+//     }
+// },2000);
 
-// Callback function
-const add3 = (a,b,callback)=>{
-    const sum = a+b;
-    callback(sum);
-};
+// //fetch
+// const getData=()=>{
+//     return fetch("https://jsonplaceholder.typicode.com/users");
+// }
+// getData().then((res)=>res.json())
+// .then((data)=>console.log(data))
+// .catch((err)=>console.log(err));
 
-const display = (res)=>{
-    console.log("Callback Result:", res);
-};
-
-add3(10,20,display);
-
-// ------------------ SPREAD OPERATOR ------------------
-
-var arr1 = [1,2,3];
-var arr2 = [...arr1,4,5,6];
-console.log(arr2);
-
-// ------------------ DESTRUCTURING ------------------
-
-var [m1,m2,m3] = [99,98,97];
-console.log(m1,m2,m3);
-
-var {name,age} = {
-    name:"usha",
-    age:24
-};
-console.log(name,age);
-
-// ------------------ MAP FILTER REDUCE ------------------
-
-var arr3 = [1,2,3,4,5];
-
-// map
-var double = arr3.map(val => val*2);
-console.log(double);
-
-// filter
-var even = arr3.filter(val => val % 2 === 0);
-console.log(even);
-
-// reduce
-var total = arr3.reduce((sum,val)=> sum+val,0);
-console.log(total);
-
-// chaining
-var finalResult = arr3
-    .map(val => val**2)
-    .filter(val => val % 2 === 0)
-    .reduce((sum,val)=> sum+val,0);
-
-console.log("Final Result:", finalResult);
+// // async and await
+// const getData=async()=>{
+//     try{
+//         const res=await fetch("https://jsonplaceholder.typicode.com/users");
+//         const data=await res.json();
+//         console.log(res);
+//     }catch(arr){
+//         console.log(arr)
+//     }
+// }
+// getData();
